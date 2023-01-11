d2d76aed8fdd3f15997c4d768d454ebacf081e45
K Gray <64373011+Kgray44@users.noreply.github.com>
2023-01-11 08:28:32 -0500

    Added Description

--- eggincubator.ino
+++ eggincubator.ino
@@ -1,3 +1,32 @@
+/*************************************************************
+File Name: eggincubator.ino
+
+Processor/Platform: Arduino Mega (DFRduino Mega tested)
+
+Development Environment: Arduino 2.0.3
+
+Download latest code here:
+https://github.com/Kgray44/Arduino-Egg-Incubator
+
+Driveway Gates code meant to be used alongside the tutorial found here:
+https://www.hackster.io/k-gray/chicken-egg-incubator-with-arduino-44f70f
+
+Copyright 2023 K Gray
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE 
+WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR 
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Liscense found here:
+https://opensource.org/licenses/MIT
+ *************************************************************/
+
 #include "DFRobot_RGBLCD1602.h"
 #include "DFRobot_AHT20.h"
 #include <AccelStepper.h>
